647b63c99075fe998e34fdbff860b7c1a2543e2c
Mmmr7 <ol-sidorenkov@mail.ru>
2022-07-03 17:17:07 +0300

    Fun

---
+++ src/constructors.rs
@@ -0,0 +1,4 @@
+pub mod constructors {
+    impl crate::metal::Matrix<T> {
+    }
+}
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,18 @@
-pub struct Matrix<T> {
-    rows_number: usize,
-    columns_number: usize,
-    matr: Vec<Vec<T>>,
+pub mod metal {
+    pub struct Matrix<T> {
+        rows_number: usize,
+        columns_number: usize,
+        matr: Vec<Vec<T>>,
+    }
+    impl Matrix<T> {
+        fn sum(&self) -> T {
+            let mut ans: T = 0;
+            for i in 0..self.rows_number {
+                for j in 0..self.columns_number {
+                    ans += self.matr[i][j];
+                }
+            }
+            return ans;
+        }
+    }
 }
