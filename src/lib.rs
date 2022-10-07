4076dbd859a513105ebfc40af066a8a6a215f473
Mmmr7 <ol-sidorenkov@mail.ru>
2022-10-08 00:14:47 +0300

    base

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
-mod matrix;
 mod types;
 mod general;
 mod indexer;
 
 #[cfg(test)]
 mod tests {
-    use crate::types;
+    use crate::types::{Matrix, BaseMatrix};
 
     #[test]
     fn check_new() {
-        let m1 = types::Matrix::new();
-        let m2 = types::Matrix::new();
+        let m1 = BaseMatrix::new();
+        let m2 = BaseMatrix::new();
         assert_eq!(m1, m2);
     }
 }
